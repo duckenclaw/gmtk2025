d6896061c3062c40b8f15119dc844f2746c6a00c
DuckenClaw <sen@duckenclaw.xyz>
2025-07-31 09:56:11 +0200

    added main menu

---
+++ scripts/main_menu.gd
@@ -0,0 +1,25 @@
+extends Control
+
+@export var start_scene: PackedScene =preload("res://scenes/test_scene.tscn")
+
+@onready var title_screen: MarginContainer = $TitleScreen
+@onready var options_screen: MarginContainer = $OptionsScreen
+
+func _on_start_button_pressed():
+	print("start")
+	get_tree().change_scene_to_packed(start_scene)
+	
+func _on_options_button_pressed():
+	toggle_options()
+	print("options")
+
+func _on_exit_options_button_pressed():
+	toggle_options()
+	print("exit options")
+
+func _on_exit_button_pressed():
+	get_tree().quit()
+
+func toggle_options():
+	title_screen.visible = !title_screen.visible
+	options_screen.visible = !options_screen.visible
