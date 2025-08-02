821f13f3efaaf40aa62e22ecda5ac99f2f3bbe1c
DuckenClaw <sen@duckenclaw.xyz>
2025-08-02 12:08:35 +0200

    added textures for crosshair and black hole, added black hole functionality in black_hole.gd

--- src/player/actions/black_hole/black_hole.gd
+++ src/player/actions/black_hole/black_hole.gd
@@ -1,8 +1,19 @@
 extends Area2D
 class_name BlackHole
 
+const BLACK_HOLE_IMPULSE = 1.0
+const BLACK_HOLE_DAMAGE = 10.0
+
+var black_hole_radius = 200
 
 func _ready() -> void:
+	$CollisionShape2D.shape.radius = black_hole_radius
 	
 	await WaitUtil.wait(5.0)
 	queue_free()
+	
+func _physics_process(delta):
+	for area in get_overlapping_areas():
+		if area.is_in_group("enemy"):
+			area.take_impulse(BLACK_HOLE_IMPULSE * area.global_position.direction_to(global_position))
+			#area.take_damage(BLACK_HOLE_DAMAGE)
--- src/player/actions/black_hole/black_hole_action.gd
+++ src/player/actions/black_hole/black_hole_action.gd
@@ -4,12 +4,16 @@ const BLACK_HOLE = preload("uid://c4xsb2ohggau7")
 
 const DAMAGE_TIMEOUT: float = 2000 # msec
 
-var last_damage_time: float = 0.0
+var last_damage_time = 0.0
 
-@onready var preview_area: CollisionShape2D = $Area2D/PreviewArea
+@export var radius = 200
+
+@onready var preview_area: Area2D = $PreviewArea
+@onready var preview_sprite: Sprite2D = $PreviewArea/Sprite2D
 @onready var player: Player = $"../.."
 
 func accept_command(command: Command):
+	print(is_reloading())
 	if command is CommandMouse:
 		preview_area.visible = not is_reloading()
 		preview_area.global_position = command.mouse_position
@@ -18,9 +22,8 @@ func accept_command(command: Command):
 			return
 		
 		var black_hole = BLACK_HOLE.instantiate()
+		black_hole.black_hole_radius = radius
 		player.get_parent().add_child(black_hole)
-		
-		last_damage_time = Time.get_ticks_msec()
 		
 		black_hole.global_position = command.mouse_position
 
