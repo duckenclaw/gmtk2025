9b244a06222e69266559900ba75c80ed5d6b9a61
DuckenClaw <sen@duckenclaw.xyz>
2025-08-02 15:00:01 +0200

    added laser action

--- src/player/actions/black_hole/black_hole_action.gd
+++ src/player/actions/black_hole/black_hole_action.gd
@@ -4,13 +4,11 @@ const BLACK_HOLE = preload("uid://c4xsb2ohggau7")
 
 const DAMAGE_TIMEOUT: float = 2000 # msec
 
-var last_damage_time: float = -100.0
+var last_damage_time = 0.0
 
 @export var radius = 200
 
 @onready var preview_area: Area2D = $PreviewArea
-@onready var preview_shape: CollisionShape2D = $PreviewArea/CollisionShape2D
-
 @onready var preview_sprite: Sprite2D = $PreviewArea/Sprite2D
 @onready var player: Player = $"../.."
 
@@ -18,15 +16,12 @@ func accept_command(command: Command):
 	if command is CommandMouse:
 		preview_area.visible = not is_reloading()
 		preview_area.global_position = command.mouse_position
-		preview_shape.shape.radius = player_config.black_hole_radius
-		
 	if command is CommandMouseReleased:
 		if is_reloading():
 			return
-		last_damage_time = Time.get_ticks_msec()
 		
-		var black_hole: BlackHole = BLACK_HOLE.instantiate()
-		black_hole.player_config = player_config
+		var black_hole = BLACK_HOLE.instantiate()
+		black_hole.black_hole_radius = radius
 		player.get_parent().add_child(black_hole)
 		
 		black_hole.global_position = command.mouse_position
