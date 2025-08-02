77b0d4ea080fd9572ccefd2e0a2a589186d60f2c
DuckenClaw <sen@duckenclaw.xyz>
2025-08-02 15:39:14 +0200

    added laser action

--- src/player/actions/laser/laser_action.gd
+++ src/player/actions/laser/laser_action.gd
@@ -1,30 +1,38 @@
 extends Action
 
 const DAMAGE_TIMEOUT: float = -1 # msec
-const LASER_LENGTH: float = 2000.0
 const LASER_DAMAGE: float = 20.0
 const LASER_IMPULSE: float = 5.0
 
 var last_damage_time = 0.0
 
-@onready var laser_raycast = $LaserRaycast
+@onready var laser_area = $LaserArea
+@onready var laser_collider = $LaserArea/CollisionShape2D
 @onready var player: Player = $"../.."
 
+
+var target_group: String = "enemy"
+var targets_in_area: Array
+
 func accept_command(command: Command):
-	if command is CommandMouseReleased:
-		if is_reloading():
-			return
-		
-		laser_raycast.enabled = true
-		
-		laser_raycast.target_position = command.mouse_position
-		var enemy = laser_raycast.get_collider()
-		if enemy:
-			print(enemy)
-			if enemy.is_in_group("enemy"):
-				enemy.take_damage(LASER_DAMAGE)
+	if command is CommandMouse:
+		laser_area.rotation = command.direction.angle()
+		laser_collider.visible = command.is_pressed
+		if Time.get_ticks_msec() - last_damage_time < DAMAGE_TIMEOUT: return
+  
+		if command.is_pressed:
+			for enemy in targets_in_area:
+				enemy.take_damage(player_config.laser_damage)
 				enemy.take_impulse(LASER_IMPULSE * global_position.direction_to(enemy.global_position))
-		laser_raycast.enabled = false
 
 func is_reloading() -> bool:
 	return Time.get_ticks_msec() - last_damage_time < DAMAGE_TIMEOUT
+
+
+func _on_area_entered(area):
+	if area.is_in_group(target_group):
+		targets_in_area.append(area)
+
+
+func _on_area_exited(area):
+	targets_in_area.erase(area)
